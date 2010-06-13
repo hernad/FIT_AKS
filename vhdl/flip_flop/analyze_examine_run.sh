7798c47ddd12ea9c5841098dbb6248a96072ee9c
Ernad Husremovic <hernad@bring.out.ba>
2010-06-13 17:09:16 +0200

    bash

---
+++ vhdl/flip_flop/analyze_examine_run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+OPTIONS=" --workdir=work"
+
+mkdir work
+
+
+
+CMD="ghdl -a $OPTIONS xor_gate.vhdl"
+
+echo 1. analize: $CMD
+$CMD
+
+CMD="ghdl -a $OPTIONS flip_flop.vhdl"
+
+echo 1b. analize: $CMD
+$CMD
+
+
+
+CMD="ghdl -e $OPTIONS flip_flop"
+
+echo 2. examine: $CMD
+
+$CMD
+
+
+
+CMD="ghdl -r $OPTIONS or_test --vcd=flip_fop.vcd"
+
+echo 3. run - vcd output: $CMD
+
+$CMD
+
